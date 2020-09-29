0a2471cd51bf707c178a527077e1f7ef21dd9d40
unknown <mikaelm7777777@gmail.com>
2020-09-29 12:02:35 +0200

    modified

--- server.js
+++ server.js
@@ -2,6 +2,8 @@
 
 var express = require('express');
 var cors = require('cors');
+var multer  = require('multer');
+var upload = multer({ dest: 'uploads/' });
 
 // require and use "multer"...
 
@@ -14,8 +16,14 @@ app.get('/', function (req, res) {
      res.sendFile(process.cwd() + '/views/index.html');
   });
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.post('/api/fileanalyse', upload.single('upfile'), function(req, res){
+  const { name, size, type } = req.file;
+  res.json({
+   name,
+   type,
+   size
+  });
+
 });
 
 app.listen(process.env.PORT || 3000, function () {
